091ff13195a40494b7d2b8f53ab2169bc41b0e8e
nuruwo <axid4444@gmail.com>
2024-11-01 19:36:53 +0900

    fix

--- Editor/DarkClassGeneratorEditorWindow.cs
+++ Editor/DarkClassGeneratorEditorWindow.cs
@@ -136,7 +136,6 @@ namespace Nuruwo.Tool
 
             if (HasNameSpace)
             {
-                AppendLine();
                 Outdent();
                 AppendLine($"}}");
             }
